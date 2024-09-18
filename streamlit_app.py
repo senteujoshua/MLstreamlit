25faba01dfef482d3cc4752ad7b297d38063e446
Senteu <117025098+senteujoshua@users.noreply.github.com>
2024-09-19 00:14:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import pandas as pd
 st.title('Machine Learning App')
-st.info("This is a Recommendation App")
+st.info("This is app build models")
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv("C:/Users/Admin/Desktop/Datasets/TMDB_movie_dataset_v11.csv")
+  df
+
